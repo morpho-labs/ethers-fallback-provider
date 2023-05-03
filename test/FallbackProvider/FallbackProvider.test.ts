2a37dd8978e777983d63ec13c901c3bb68424cfb
Oumar <o_fall@orange.fr>
2023-05-03 14:23:51 +0200

    test(FallbackProvider): fix tests

--- test/FallbackProvider/FallbackProvider.test.ts
+++ test/FallbackProvider/FallbackProvider.test.ts
@@ -1,56 +1,67 @@
-import * as FallbackProviderApi from "../../src/FallbackProvider";
+import { FallbackProvider } from "src/FallbackProvider";
+
+import { BaseProvider } from "@ethersproject/providers";
 
 import FailingProvider from "./helpers/FailingProvider";
 import MockProvider from "./helpers/MockProvider";
 
-jest.spyOn(FallbackProviderApi, "checkNetworks").mockImplementation();
-
 describe("FallbackProvider", () => {
-  it("should return the first value if the first provider is successful", async () => {
-    const provider1 = new MockProvider("1");
-    const provider2 = new MockProvider("2");
-    const provider = new FallbackProviderApi.FallbackProvider([provider1, provider2]);
+  describe("perform", () => {
+    jest.mock("../../src/FallbackProvider", () => ({
+      ...jest.requireActual("../../src/FallbackProvider"),
+      checkNetworks: jest
+        .fn()
+        .mockImplementation(async (providers: BaseProvider[]) =>
+          providers[0].getNetwork().then((network) => ({ network, providers }))
+        ),
+    }));
 
-    jest.spyOn(provider1, "perform");
-    jest.spyOn(provider2, "perform");
+    it("should return the first value if the first provider is successful", async () => {
+      const provider1 = new MockProvider("1");
+      const provider2 = new MockProvider("2");
+      const provider = new FallbackProvider([provider1, provider2]);
 
-    const res = await provider.perform("send", {});
+      jest.spyOn(provider1, "perform");
+      jest.spyOn(provider2, "perform");
 
-    expect(provider1.perform).toHaveBeenCalledTimes(1);
-    expect(provider2.perform).not.toHaveBeenCalled();
-    expect(res).toEqual("1");
-  });
+      const res = await provider.perform("send", {});
 
-  it("should return the second value if the first provider is failing", async () => {
-    const provider1 = new FailingProvider("1");
-    const provider2 = new MockProvider("2");
-    const provider = new FallbackProviderApi.FallbackProvider([provider1, provider2]);
+      expect(provider1.perform).toHaveBeenCalledTimes(1);
+      expect(provider2.perform).not.toHaveBeenCalled();
+      expect(res).toEqual("1");
+    });
 
-    jest.spyOn(provider1, "perform");
-    jest.spyOn(provider2, "perform");
+    it("should return the second value if the first provider is failing", async () => {
+      const provider1 = new FailingProvider("1");
+      const provider2 = new MockProvider("2");
+      const provider = new FallbackProvider([provider1, provider2]);
 
-    const res = await provider.perform("send", {});
+      jest.spyOn(provider1, "perform");
+      jest.spyOn(provider2, "perform");
 
-    expect(provider1.perform).toHaveBeenCalledTimes(1);
-    expect(provider2.perform).toHaveBeenCalledTimes(1);
-    expect(res).toEqual("2");
-  });
+      const res = await provider.perform("send", {});
 
-  it("should fail if all providers are failing", async () => {
-    const provider1 = new FailingProvider("1");
-    const provider2 = new FailingProvider("2");
-    const provider = new FallbackProviderApi.FallbackProvider([provider1, provider2]);
+      expect(provider1.perform).toHaveBeenCalledTimes(1);
+      expect(provider2.perform).toHaveBeenCalledTimes(1);
+      expect(res).toEqual("2");
+    });
 
-    jest.spyOn(provider1, "perform");
-    jest.spyOn(provider2, "perform");
+    it("should fail if all providers are failing", async () => {
+      const provider1 = new FailingProvider("1");
+      const provider2 = new FailingProvider("2");
+      const provider = new FallbackProvider([provider1, provider2]);
 
-    try {
-      await provider.perform("send", {});
-    } catch (e) {
-      expect(e).toEqual(new Error("Failing provider used: 2"));
-    }
+      jest.spyOn(provider1, "perform");
+      jest.spyOn(provider2, "perform");
 
-    expect(provider1.perform).toHaveBeenCalledTimes(1);
-    expect(provider2.perform).toHaveBeenCalledTimes(1);
+      try {
+        await provider.perform("send", {});
+      } catch (e) {
+        expect(e).toEqual(new Error("Failing provider used: 2"));
+      }
+
+      expect(provider1.perform).toHaveBeenCalledTimes(1);
+      expect(provider2.perform).toHaveBeenCalledTimes(1);
+    });
   });
 });
